1d893e72deb59bb5d3725983237057d572d622c7
vishwajeet0304 <ch.vishu25@gmail.com>
2026-05-09 20:48:34 +0530

    Fix frontend page import casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
